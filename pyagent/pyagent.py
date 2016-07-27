1cee20225a701e723f64775131834c25ae4eff32
Alex H. Wagner <awagner@genome.wustl.edu>
2016-07-27 11:40:45 -0500

    add driver class skeleton

---
+++ pyagent/pyagent.py
@@ -0,0 +1,7 @@
+class Driver:
+
+    def __init__(self, user_id=None, **kwargs):
+        # TODO: Initialize instance variables here (DL#, SSN, DoB, name, etc) as provided in kwargs
+        # TODO: Make containers (or containers of containers) for residency, vehicles, accidents, violations, discounts,
+        #       and other drivers (other drivers should be Driver objects)
+        pass
